91c8ad86f28b056d54582a076a60618ea6b4c864
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-08-18 23:01:18 +0200

    change menu component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [RouterOutlet],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
