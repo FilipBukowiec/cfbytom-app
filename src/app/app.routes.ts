2764f56b063261e6deb6fc10491879c9d1ee5035
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-08-18 22:31:26 +0200

    add login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,17 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { AdminComponent } from './admin/admin.component';
 import { NewsComponent } from './news/news.component';
 import { MediaComponent } from './media/media.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     component: HomepageComponent,
   },
-
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
   {
     path: 'admin',
     component: AdminComponent,
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MenuComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
