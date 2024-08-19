1791b2377b6e75b55faa739315c5f7230269130e
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-08-19 15:41:23 +0200

    changes main.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
-import { MenuComponent } from '../menu/menu.component';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  loginObj: any = {
+    "EmailId": "",
+    "Password": ""
+  }
+
+
+
+  http = inject(HttpClient);
+
+  constructor(private router:Router){
+
+  }
+
+  onLogin() {
+    this.http.post("https://freeapi.miniprojectideas.com/api/User/Login", this.loginObj).subscribe((res: any) => {
+      if (res.result) {
+        alert("login succes");
+        this.router.navigateByUrl("admin")
+      } else {
+        alert("check email or password");
+      }
+    })
+  }
 }
