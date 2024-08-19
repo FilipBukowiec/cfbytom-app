49d985321b788102e76737a7a8cd99f299f8121a
FilipBukowiec <102471937+FilipBukowiec@users.noreply.github.com>
2024-08-19 12:40:24 +0200

    add fullscreen

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -9,5 +9,44 @@ import { RouterModule } from '@angular/router';
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+  isFullscreen: boolean = false;
 
+  toggleFullscreen(): void {
+    if (!this.isFullscreen) {
+      this.openFullscreen();
+    } else {
+      this.closeFullscreen();
+    }
+    this.isFullscreen = !this.isFullscreen;
+  }
+
+  openFullscreen(): void {
+    const elem: any = document.documentElement;
+
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    }
+    // } else if (elem.mozRequestFullScreen) { /* Firefox */
+    //   elem.mozRequestFullScreen();
+    // } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    //   elem.webkitRequestFullscreen();
+    // } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    //   elem.msRequestFullscreen();
+    // }
+  }
+
+  closeFullscreen(): void {
+    const doc: any = document;
+
+    if (doc.exitFullscreen) {
+      doc.exitFullscreen();
+    }
+    // } else if (doc.mozCancelFullScreen) { /* Firefox */
+    //   doc.mozCancelFullScreen();
+    // } else if (doc.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    //   doc.webkitExitFullscreen();
+    // } else if (doc.msExitFullscreen) { /* IE/Edge */
+    //   doc.msExitFullscreen();
+    // }
+  }
 }
